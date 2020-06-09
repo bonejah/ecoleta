50e0d25a76c421ca6f0f94b1eb7db78bbed0c1ab
Bruno Lima <brunopclima81@gmail.com>
2020-06-08 22:54:03 -0300

    finish project ecoleta

--- src/server.js
+++ src/server.js
@@ -2,27 +2,89 @@ const express = require("express");
 
 const server = express();
 
+const db = require("./database/db");
+
 // configurar pasta publica
-server.use(express.static("public"))
+server.use(express.static("public"));
+
+// habilita o uso do req.body na aplicação 
+server.use(express.urlencoded({ extended: true }))
 
 // utilizando template engine
-const nunjucjks = require("nunjucks")
+const nunjucjks = require("nunjucks");
 nunjucjks.configure("src/views", {
   express: server,
-  noCache: true
-}) 
+  noCache: true,
+});
 
 // configurar caminhos da aplicação pagina inicial
 server.get("/", (req, res) => {
-  return res.render("index.html")
+  return res.render("index.html");
 });
 
 server.get("/create-point", (req, res) => {
-  return res.render("create-point.html")
+  return res.render("create-point.html");
+});
+
+server.post("/save-point", (req, res) => {
+
+  console.log(req.body )
+
+  const query = ` INSERT INTO places (
+    image,
+    name,
+    address,
+    address2,
+    state,
+    city, 
+    items
+  ) VALUES (?, ?, ?, ?, ?, ?, ?);`
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]  
+
+  function afterInsertData(err) {
+    if (err) {
+      console.log(err)
+      return res.send("Erro no cadastro")
+    }
+
+    console.log("Cadastrado com sucesso.")
+    console.log(this)
+    return res.render("create-point.html", { saved: true })
+  }
+
+  db.run(query, values, afterInsertData) 
+
 });
 
 server.get("/search", (req, res) => {
-  return res.render("search-results.html")
+  const search = req.query.search
+
+  if (search == "") {
+    return res.render("search-results.html", { places: rows, total: 0 });
+  }
+
+
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Retornando registros: ");
+    console.log(rows);
+
+    const total = rows.length
+
+    return res.render("search-results.html", { places: rows, total: total });
+  });
 });
 
 // liga o servidor
